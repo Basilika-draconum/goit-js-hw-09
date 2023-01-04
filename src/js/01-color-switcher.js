09ceecf7d6a24986ea8cc5110389733d629d5d6b
Lika <14dubinenkoanzhelika@gmail.com>
2023-01-04 15:42:59 +0200

    add task -01.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const refs = {
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
+let timerId = null;
 
+refs.btnStart.addEventListener('click', onBtnStartClick);
+
+refs.btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function onBtnStartClick(e) {
+  if (timerId !== null) {
+    clearInterval(timerId);
+  }
+  const value = getRandomHexColor();
+  document.body.style.backgroundColor = value;
+  timerId = setInterval(onBtnStartClick, 1000);
+}
